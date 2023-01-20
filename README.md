ad50f9452ef010be3c5e9aed97c302663d8b41a7
callmepvp <mrxgameing@gmail.com>
2023-01-20 18:11:04 +0200

    encoding

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # Cozy Grove Save Editor
-Currently only decodes the .sf data file to readable json and encodes the json back to a save file 
+Currently only decodes the .sf data file to readable json and encodes the json back to a save file
+
 All data manipulation will have to be done by the user
+
+Doesn't encode data properly yet
 
 I'm not responsible for any kind of data loss, back up your saves!
--- index.py
+++ index.py
@@ -14,7 +14,7 @@ def validPath(filepath):
 layout = [
             [sg.Text("Cozy Grove Save Editor (WIP)")],
             [sg.Text("Created by callmepvp (pvp#7272)")],
-            [sg.Text("File Location:"), sg.Input(key="-IN-"), sg.FileBrowse(file_types=(("Save Files", "*.sf", "*.json"),))],
+            [sg.Text("File Location:"), sg.Input(key="-IN-"), sg.FileBrowse(file_types=[("Save Files", "*.sf"), ("Json Files", "*.json")])],
             [sg.Text("Output Location:"), sg.Input(key="-OUT-"), sg.FolderBrowse()],
             [sg.Button("Decode")], [sg.Button("Encode")]
 ]
@@ -49,36 +49,29 @@ while True:
             json_str=json.loads(decodedStr)
 
             #Output Formatted File
-            with open(f'{outputFilePath}/output/output.json', 'w') as f:
+            with open(f'{outputFilePath}/cg_save.json', 'w') as f:
                 json.dump(json_str, f, indent=4)
 
             print('[DECODING SUCCESSFUL]')
-            
-    if event == "Encode":
+
+    elif event == "Encode":
         if validPath(values["-IN-"]) and validPath(values["-OUT-"]):
 
             inputFilePath = values["-IN-"]
             outputFilePath = values["-OUT-"]
 
             with open(inputFilePath, "r") as dataFile:
-                dataList = dataFile.readlines()
+                data = json.load(dataFile)
+                Text = json.dumps(data)
 
-            Text = ""
-            for items in dataList:
-                Text = Text + items +"\n"
-
-            Text = Text[4:] #Remove the decryption block
-
-            #Format File
-            decodedBytes = base64.b64decode(Text)
-            decodedStr = decodedBytes.decode("ascii") 
-            json_str=json.loads(decodedStr)
+                #Format File
+                encodedBytes = base64.b64encode(Text.encode('utf-8'))
 
             #Output Formatted File
-            with open(f'{outputFilePath}/output/output.json', 'w') as f:
-                json.dump(json_str, f, indent=4)
+            with open(f'{outputFilePath}/cg_save.sf', 'wb') as f:
+                f.write(encodedBytes)
 
-            print('[DECODING SUCCESSFUL]')
+            print('[ENCODING SUCCESSFUL]')
 
     elif event == sg.WIN_CLOSED:
         break
